90337b9ae30bba7ac9f5de09f2bf915b9bca67bb
DenysMkl <den.mikliaiev@gmail.com>
2024-06-05 17:39:37 +0300

    implement changes

--- src/counter.py
+++ src/counter.py
@@ -16,12 +16,26 @@ class ContextManager:
 
 
 def count_words(filename):
+    """Retrieves data from the selected file and counts
+        the number of times each word is mentioned in that file.
+
+    Returns dictionary that stores the calculation results.
+    The keys in the dictionary are the words themselves,
+    and the values are the number of times each word appears in the file.
+    """
     with ContextManager(filename) as file:
         text_of_words = file.read_file()
-        list_of_words = text_of_words.split()
-        dict_word_count = dict()
-        for word in set(list_of_words):
-            count_of_word = list_of_words.count(word)
-            dict_word_count.update({word: count_of_word})
+
+    list_of_words = text_of_words.split()
+    list_of_words = list(map(lambda x: x.rstrip(',. '), list_of_words))
+    dict_word_count = dict()
+
+    for word in set(list_of_words):
+        count_of_word = list_of_words.count(word)
+        dict_word_count.update({word: count_of_word})
 
     return dict_word_count
+
+
+if __name__ == '__main__':
+    print(count_words('text.txt'))
